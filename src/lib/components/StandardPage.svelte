dc49bdf1b2635771e7318cd4cb1fc87721b881f2
Vasculum <50236924+Vasculum@users.noreply.github.com>
2024-01-03 10:48:53 +0000

    commit and create new block

--- src/lib/components/StandardPage.svelte
+++ src/lib/components/StandardPage.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	export let data;
+	import '$lib/Accordion.svelte' as accordionComponent;
 </script>
 
+{#each data.entryData.composer as composer}
+	<svelte:component this={composer.type} />
+{/each}
+
+<!--
 <h1>{data.entryData.pageHeading || ''}</h1>
 <p>{@html data.entryData.richText || ''}</p>
 <p>{data.entryData.longText || ''}</p>
-
+-->
 <style>
 	h1 {
 		font-size: 2.5em;
