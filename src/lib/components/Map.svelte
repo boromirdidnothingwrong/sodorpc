025e453122827b705acbb6d068c51b5b9a27ba6f
Vasculum <50236924+Vasculum@users.noreply.github.com>
2024-01-19 11:12:47 +0000

    Added map support

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,15 +1,3 @@
 <script lang="ts">
-	export let coords;
+	export let url;
 </script>
-
-<iframe
-	title="Map of Sodor Parish Council"
-	width="450"
-	height="250"
-	frameborder="0"
-	style="border:0"
-	referrerpolicy="no-referrer-when-downgrade"
-	src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDDCQ8VBj7tlbrRd4M6-xEMI4_h3BrE_b4&q=Eiffel+Tower,Paris+France"
-	allowfullscreen
->
-</iframe>
--- src/lib/components/StandardPage.svelte
+++ src/lib/components/StandardPage.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	export let data;
 	import Image from '$lib/components/Image.svelte';
+	import Map from '$lib/components/Map.svelte';
 </script>
 
 {#each data.entryData.composer as composer}
@@ -19,18 +20,18 @@
 			{/each}
 		{:else if composer.type == 'richText'}
 			{#each composer.value as value}
-				<div>{@html value}</div>
+				{@html value}
 			{/each}
-		{:else if composer.type == 'location'}
-			<p style="text-color: red">
-				If you can't see a map here, the web developer hasn't finished implementing the feature.
-			</p>
 		{:else if composer.type == 'image'}
 			{#each composer.value as value}
 				{#if composer.value.asset != null}
 					<Image URI={value.asset.sys.id} />
 				{/if}
 			{/each}
+		{:else if composer.type == 'map'}
+			{@const map = composer.value}
+
+			{@html composer.value}
 		{/if}
 	</div>
 {/each}
